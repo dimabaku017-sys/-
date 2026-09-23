6d4d238f139e26c62a2b6feead98754ea7882882
dimabaku017-sys <dimabaku017@gmail.com>
2026-09-23 15:49:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,19 @@
-self.addEventListener('install', (e) => {
+const CACHE = "diary-v1";
+const FILES = ["/ -/", "/-/index.html", "/-/icon.png", "/-/manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(FILES))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener("activate", (e) => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  // Просто пропускаем все запросы
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
 });
